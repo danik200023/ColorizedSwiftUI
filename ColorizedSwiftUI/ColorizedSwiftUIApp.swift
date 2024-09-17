1e62f739d76bf97d61ee5512e2614f8f9425acd6
danila <danik200023@proton.me>
2024-09-17 19:31:44 +0400

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Данила Умнов on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
